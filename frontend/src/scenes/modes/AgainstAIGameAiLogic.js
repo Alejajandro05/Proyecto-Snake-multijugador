266925523b418fe319bbb68c56db0fa0e7ded297
Bencrex <klee.benjamin6@gmail.com>
2026-05-15 18:26:56 +0100

    only missing avoiding obstacules till first test of AI

--- frontend/src/scenes/modes/AgainstAIGameAiLogic.js
+++ frontend/src/scenes/modes/AgainstAIGameAiLogic.js
@@ -11,13 +11,21 @@ const SNAKE_BOARD_TILE = {
     obstacle: 6,
 };
 
+const SNAKE_DIRECTIONS = {
+    up: "up",
+    left: "left",
+    right: "right",
+    down: "down"
+}
+
 export const againstAIGameAiLogic = {
     targetApple: {y: null, x: null},
 
     calculateAINextDirection(oldState, engine) {
         const snakeboard = this.fetchsnakeboard(oldState, engine);
 
-        const direction = this.calculateAINextDirectionWithSnakeboard(snakeboard);
+        //TODO if (P2_ID is alive) do else  don't do
+        const direction = this.calculateAINextDirectionWithSnakeboard(snakeboard, oldState.players.get(P2_ID).direction);
 
         engine.setNextDirection(P2_ID, "up"/*direction*/);//TODO use direction
 
@@ -68,9 +76,9 @@ export const againstAIGameAiLogic = {
         return snakeboard;
     },
 
-    calculateAINextDirectionWithSnakeboard(snakeboard) {
+    calculateAINextDirectionWithSnakeboard(snakeboard, currentAiDirection) {
         this.targetApple = this.findRandomTargetApple(snakeboard);
-        return this.calculateAINextDirectionWithSnakeboardAndTargetApple(snakeboard, this.targetApple);
+        return this.calculateAINextDirectionWithSnakeboardAndTargetApple(snakeboard, this.targetApple, currentAiDirection);
     },
 
     findRandomTargetApple(snakeboard) {
@@ -99,18 +107,88 @@ export const againstAIGameAiLogic = {
         throw error("AI couldn't find Target Apple. No apple exist in the snakeboard!");
     },
 
-    calculateAINextDirectionWithSnakeboardAndTargetApple(snakeboard, targetApple) {
-        var direction = this.calculateDirectionToTargetApple(snakeboard, targetApple);
-        direction = this.avoidObstaculeOrEnemy(snakeboard, direction);
-        return direction;
+    calculateAINextDirectionWithSnakeboardAndTargetApple(snakeboard, targetApple, currentAiDirection) {
+        const aiPlayerHead = this.findAiPlayerHead(snakeboard);
+        const targetAppleDirection = this.calculateDirectionToTargetApple(snakeboard, aiPlayerHead, targetApple, currentAiDirection);
+        return this.avoidObstaculeOrEnemy(snakeboard, aiPlayerHead, targetAppleDirection, currentAiDirection);
     },
 
-    calculateDirectionToTargetApple(snakeboard, targetApple) {
-
+    findAiPlayerHead(snakeboard) {
+        for (let y = 0; y < snakeboard.length; y++) {
+            for (let x = 0; x < snakeboard.length; x++) {
+                if (snakeboard[y][x] == SNAKE_BOARD_TILE.aiPlayerHead) {
+                    return {y:y, x:x};
+                }
+            }
+        }
+        throw error("AI Player Head not found!");
     },
 
-    avoidObstaculeOrEnemy(snakeboard) {
+    calculateDirectionToTargetApple(snakeboard, aiPlayerHead, targetApple, currentAiDirection) {
+        if (aiPlayerHead.x === targetApple.x) {//TODO avoiding an obstacle could cause problems when requiring x to be the same
+            if (this.verticalBorderDirectionFaster(snakeboard, aiPlayerHead, targetApple)) {
+                return (aiPlayerHead.y-targetApple.y < 0) ? SNAKE_DIRECTIONS.up : SNAKE_DIRECTIONS.down;
+            }
+            else {
+                return (aiPlayerHead.y-targetApple.y < 0) ? SNAKE_DIRECTIONS.down : SNAKE_DIRECTIONS.up;
+            }
+        }
+        else {
+            if (this.horizontalBorderDirectionFaster(snakeboard, aiPlayerHead, targetApple)) {
+                return (aiPlayerHead.x-targetApple.x < 0) ? SNAKE_DIRECTIONS.left : SNAKE_DIRECTIONS.right;
+            }
+            else {
+                return (aiPlayerHead.x-targetApple.x < 0) ? SNAKE_DIRECTIONS.right : SNAKE_DIRECTIONS.left;
+            }
+        }
+    },
 
+    horizontalBorderDirectionFaster(snakeboard, aiPlayerHead, targetApple) {
+        return !(snakeboard[0].length/2 < aiPlayerHead.x-targetApple.x && aiPlayerHead.x-targetApple.x < snakeboard[0].length/2);
+    },
+
+    verticalBorderDirectionFaster(snakeboard, aiPlayerHead, targetApple) {
+        return !(snakeboard.length/2 < aiPlayerHead.y-targetApple.y && aiPlayerHead.y-targetApple.y < snakeboard.length/2);
+    },
+
+    avoidObstaculeOrEnemy(snakeboard, aiPlayerHead, targetAppleDirection, currentAiDirection) {
+        if (!this.obstaculInAISnakeDirection(snakeboard, aiPlayerHead, targetAppleDirection)) {
+            return targetAppleDirection;
+        }
+        else {
+
+        }
+    },
+
+    obstaculInAISnakeDirection(snakeboard, aiPlayerHead, nextSnakeDirection) {
+        let nextX = aiPlayerHead.x;
+        let nextY = aiPlayerHead.y;
+
+        // count Direciton one up
+        if (nextSnakeDirection == SNAKE_DIRECTIONS.up) {
+            nextY += 1;
+        }
+        else if (nextSnakeDirection == SNAKE_DIRECTIONS.left) {
+            nextX -= 1;
+        }
+        else if (nextSnakeDirection == SNAKE_DIRECTIONS.right) {
+            nextX += 1;
+        }
+        else if (nextSnakeDirection == SNAKE_DIRECTIONS.down) {
+            nextY += 1;
+        }
+
+        // next move to border appears at other side
+        if (nextY >= snakeboard.length)
+            nextY = 0;
+        else if (nextY < 0)
+            nextY = snakeboard.length;
+        if (nextX >= snakeboard[0].length)
+            nextX = 0;
+        else if (nextX < 0)
+            nextX = snakeboard[0].length;
+
+        return (snakeboard[nextY][nextX] != SNAKE_BOARD_TILE.empty);
     },
 
     logSnakeboard(snakeboard) {
